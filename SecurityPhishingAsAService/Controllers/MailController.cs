e39054bf36e396ad4f5be56ded40a9e26df68b5d
Gideon <30596579+DarkIodine@users.noreply.github.com>
2021-02-24 12:42:35 +0100

    Initial Commit

---
+++ SecurityPhishingAsAService/Controllers/MailController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SecurityPhishingAsAService.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class MailController : ControllerBase
+    {
+        [HttpPost("SendMailTo/{from}/{subject}/{body}")]
+        public bool SendMail(string email, string from, string body, string subject)
+        {
+            try
+            {
+                var smtpClient = new SmtpClient("localhost")
+                {
+                    Port = 25
+                };
+                smtpClient.Send(from, email, subject, body);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+        [HttpPost("SendMailToBulk/{from}/{subject}/{body}")]
+        public bool SendMailInBulk(List<string> emailList, string from, string body, string subject)
+        {
+            try
+            {
+                foreach (var email in emailList)
+                {
+                    try
+                    {
+                        var smtpClient = new SmtpClient("localhost")
+                        {
+                            Port = 25
+                        };
+                        smtpClient.Send(from, email, subject, body);
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+ 
+        }
+    }
+}
